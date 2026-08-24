41c434a8310f7ca0a2b83a514afa8fbb265e4104
GiantPreston <146558151+giantpreston@users.noreply.github.com>
2026-08-23 22:29:54 -0300

    fix: patch getInfo

--- endpoints/getInfo.js
+++ endpoints/getInfo.js
@@ -4,6 +4,6 @@ module.exports = {
     method: 'get',
     path: '/switcher/getInfo.php',
     handler: (req, res) => {
-        res.send(`{"motd": "${config.motd}", "version": 1, "icon": "${config.icon}"}`);
+        res.json({motd: config.motd ?? '',version: 1,icon: config.icon ?? ''}); // fuck this shit
     }
 };
